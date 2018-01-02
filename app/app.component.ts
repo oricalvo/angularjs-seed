1ed36afa3416879cf931353baa3b191481a0c1dd
Alon <od120162@gmail.com>
2018-01-02 22:10:08 +0200

    webpack.config.json updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import {appModule} from "./app.module";
 import template from "./app.component.html";
+// import  style  from "./app.component.style.css";
+import  url from "./app.component.style.css";
 
 export class AppComponent {
     constructor() {
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
             {
                 test: /\.html$/,
                 use: 'raw-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
             }
         ]
     }
