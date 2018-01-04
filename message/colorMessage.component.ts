8fe5cf9a6d5f52b2201c0422ab3c866c855a4980
Alon <od120162@gmail.com>
2018-01-04 18:01:28 +0200

    working

--- message/colorMessage.component.ts
+++ message/colorMessage.component.ts
@@ -34,15 +34,6 @@ const getRandom256 = (): number => {
     return Math.floor((Math.random() * 256) + 0);
 }
 
-const padLeft = function (str: string, paddingChar: string, length: number) {
-    if ((str.length < length) && (paddingChar.toString().length > 0)) {
-        for (var i = 0; i < (length - this.length); i++) {
-            str = paddingChar.toString().charAt(0).concat(str);
-        }
-    }
-    return str;
-}
-
 const pad = (str: string, size: number) => {
     while (str.length < size) {
         str = '0' + str;
