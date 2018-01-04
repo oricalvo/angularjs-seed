feb0f93a062e2b5149c29b06676da19033dd32df
Alon <od120162@gmail.com>
2018-01-04 05:39:09 +0200

    ..

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,5 @@
 import * as angular from 'angular';
 import { appModule } from './app.module';
-// import {AppComponent} from "./app.component";
 import './site.css';
-
-// const components = [
-//     AppComponent,
-// ];
 
 angular.bootstrap(document.querySelector('html'), [appModule.name]);
--- message/message.component.ts
+++ message/message.component.ts
@@ -1,20 +1,33 @@
 import { appModule } from '../app/app.module';
 import template from './message.component.html';
 import './message.component.css';
+import * as angular from 'angular';
+// import * as rgbHex from 'rgb-hex';
 
 export class AppComponent {
-    color: string;
+    bgColor: string;
 
     constructor() {
-        this.color = 'red45';
+        const rgbColor =
+            window.getComputedStyle(document.body, null)
+                .getPropertyValue('background-color');
+        this.bgColor = rgb2hex(rgbColor);
     }
 
-    setColorMessage(color: string) {
-        this.color = color;
+    rndColor() {
+        const rgbColor = `rgb(${getRandom256()}, ${getRandom256()}, ${getRandom256()})`;
+        document.body.style.backgroundColor = rgbColor;
+        this.bgColor = rgb2hex(rgbColor);
     }
 }
 
-// appModule.component("appRoot", {
-//     controller: AppComponent,
-//     template: template,
-// });
+const rgb2hex = (rgb: string): string => {
+    const res = '#' + rgb.substr(4, rgb.indexOf(')') - 4)
+        .split(',')
+        .map((color) => parseInt(color).toString(16)).join('');
+    return res;
+}
+
+const getRandom256 = (): number => {
+    return Math.floor((Math.random() * 256) + 0);
+}
